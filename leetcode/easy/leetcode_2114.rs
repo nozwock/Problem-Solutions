5f1dc84d257aa9c7b0c7f1ba7455a41ccb2fda85
nozwock <57829219+nozwock@users.noreply.github.com>
2022-02-19 17:30:11 +0530

    fix: forgot to add logic for max words

--- leetcode/easy/leetcode_2114.rs
+++ leetcode/easy/leetcode_2114.rs
@@ -5,7 +5,10 @@ impl Solution {
     pub fn most_words_found(sentences: Vec<String>) -> i32 {
         let mut most_words: i32 = 0;
         for (_i, x) in sentences.iter().enumerate() {
-            most_words = x.split(" ").collect::<Vec<&str>>().len() as i32;
+            let mut _num_words: i32 = x.split(" ").collect::<Vec<&str>>().len() as i32;
+            if _num_words > most_words {
+                most_words = _num_words;
+            }
         }
         return most_words;
     }
@@ -15,9 +18,9 @@ fn main() {
     println!(
         "{:?}",
         Solution::most_words_found(vec![
+            "this is great thanks very much".to_string(),
             "alice and bob love leetcode".to_string(),
             "i think so too".to_string(),
-            "this is great thanks very much".to_string(),
         ])
     );
 }
